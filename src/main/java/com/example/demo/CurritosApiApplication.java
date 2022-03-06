e624b318cd6744ee1c06f8104193f3acc71156c0
JavierCamposCuesta <javiercamposcuesta@gmail.com>
2022-03-06 15:34:16 +0100

    Añado spring boot initializater

--- src/main/java/com/example/demo/CurritosApiApplication.java
+++ src/main/java/com/example/demo/CurritosApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurritosApiApplication {
+public class CurritosApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurritosApiApplication.class, args);
